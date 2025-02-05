7c129d8a2a2827c8c3ca04778fd095b9d8cf21a4
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-06 02:51:53 +0600

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { createArticle, getArticles, getArticle, addComment, getComments } = require('../controllers/articleController');
 const auth = require('../middleware/auth');
+const upload = require('../config/multer');
 
-router.post('/', auth, createArticle);
+router.post('/', auth, upload.single('image'), createArticle);
 router.get('/', getArticles);
 router.get('/:id', getArticle);
 router.post('/comments', auth, addComment);
